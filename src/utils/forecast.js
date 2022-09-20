7bbf7eda502dd43a92b9b9d9ae8eebf63b72c790
Weerathit <weerathit.s@gmail.com>
2022-09-20 20:19:02 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=b2d4f139daacd1eaf6e2e3528c84637c&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
